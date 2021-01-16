335eab894e00977c41affe0d908106e7fbc2f5c1
nschuler <nicholas.schuler@student.uts.edu.au>
2021-01-17 01:11:00 +1100

    Added Header and assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tesla Home Clone</h1>
+      <Header />
+        {/* items => snapping */}
+        {/* items => snapping */}
+        {/* items => snapping */}
+        {/* items => snapping */}
     </div>
   );
 }
