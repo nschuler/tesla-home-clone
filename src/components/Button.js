ac66f5a2bdd8112b421e6213ef824ab0bb23a954
nschuler <nicholas.schuler@student.uts.edu.au>
2021-01-17 01:51:33 +1100

    Semi complete Item Component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ imp, text, link }) => {
+    return (
+        <div className='button'>
+            
+        </div>
+    )
+}
+
+export default Button
